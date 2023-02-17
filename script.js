586293701f7ffbf38e62559bf5a1029689e451ed
yusufINC <75489069+yusufinc@users.noreply.github.com>
2023-02-17 00:02:21 +0000

    ajax ile istek gönderdik

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Şifremi unuttum linkine tıklandığında şifre sıfırlama talebi gönderilmesi için
+// bir AJAX isteği gönderiyoruz.
+
+var forgotPasswordLink = document.querySelector(".forgot-password a");
+
+forgotPasswordLink.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var username = document.getElementById("username").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/reset-password");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({username: username}));
+
+  alert("Şifre sıfırlama talebiniz alınmıştır. Lütfen e-postanızı kontrol edin.");
+});
+``
 
